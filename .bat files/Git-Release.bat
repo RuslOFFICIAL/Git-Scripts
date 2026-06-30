a50b89704771aeaf210436b7231a88f536f36f30
Rusl <ryslan14523uaiyt@gmail.com>
2026-06-30 21:26:05 +0300

    Add Git-Release

---
+++ .bat files/Git-Release.bat
@@ -0,0 +1,35 @@
+@echo off
+setlocal enabledelayedexpansion
+
+REM .conf files.
+if exist "..\.conf files\Variables.conf" (
+    for /f "usebackq eol=# tokens=1,2 delims==" %%A in ("..\.conf files\Variables.conf") do set "%%A=%%~B"
+)
+
+echo Git-Release %Git-Release_Version%&echo.
+
+REM Define paths relative to the script location.
+set "SourceDir=.."
+set "StagingDir=..\TempRelease"
+set "ZipFolder=..\Releases"
+set "ZipFile=%ZipFolder%\Git-Scripts_%Git-Scripts_Version%.zip"
+
+echo Preparing release folder (excluding all .conf files)...
+robocopy "%SourceDir%" "%StagingDir%" /E /XF *.conf /XD TempRelease Releases .git
+
+echo.
+echo Compressing into .zip file...
+REM Create the output directory if it doesn't exist.
+if not exist "%ZipFolder%" mkdir "%ZipFolder%"
+
+REM Use PowerShell to compress the staging contents.
+powershell -Command "Compress-Archive -Path '%StagingDir%\*' -DestinationPath '%ZipFile%' -Force"
+
+echo.
+echo Cleaning up temporary folders...
+rmdir /s /q "%StagingDir%"
+
+endlocal
+echo.
+echo Done! Your release is ready inside the "Releases" folder.
+pause
--- README.md
+++ README.md
@@ -4,3 +4,4 @@ I hope someone are gonna use it. :3
 Scripts:
 - Git-Push is a script to push your changes to a GitHub repository.
 - Git-Link-Repo is a script to link your local repository files to a GitHub repository.
+- Git-Release is a script to make the files of Git-Script to a .zip file without .conf files.
