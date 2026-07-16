5448425452fd48489d6aa8b234c0c69b35ad9fd4
RuslOFFICIAL <ryslan14523uaiyt@gmail.com>
2026-07-16 11:02:40 +0300

    Cosmetic update

--- .sh-files/Git-Release.sh
+++ .sh-files/Git-Release.sh
@@ -18,11 +18,11 @@ echo "Git-Release $Git_Release_Version" && echo ""
 # Paths
 SOURCE_DIR=".."
 STAGING_DIR="../TempRelease"
-ZIP_FOLDER="../Releases"
-ZIP_FILE="$ZIP_FOLDER/Git-Scripts_$Git_Scripts_Version.tar.gz"
+ARCHIVE_FOLDER="../Releases"
+ARCHIVE_FILE="$ARCHIVE_FOLDER/Git-Scripts_$Git_Scripts_Version.tar.gz"
 
 echo -n "Cleaning release folder... "
-rm -f "$ZIP_FOLDER"/Git-Scripts_*.tar.gz
+rm -f "$ARCHIVE_FOLDER"/Git-Scripts_*.tar.gz
 
 echo "Done!" && echo -n "Preparing release folder... "
 mkdir -p "$STAGING_DIR"
@@ -45,9 +45,9 @@ mkdir -p "$STAGING_DIR/.conf-files"
 cp "$VARIABLES_FILE" "$STAGING_DIR/.conf-files/"
 cp "../.conf-files/Git-Launcher_Info.conf" "$STAGING_DIR/.conf-files/"
 
-echo "Done!" && echo -n "Compressing into .zip file... "
-mkdir -p "$ZIP_FOLDER"
-tar -czf "$ZIP_FILE" -C "$STAGING_DIR" .
+echo "Done!" && echo -n "Compressing into .tar.gz file... "
+mkdir -p "$ARCHIVE_FOLDER"
+tar -czf "$ARCHIVE_FILE" -C "$STAGING_DIR" .
 
 echo "Done!" && echo -n "Cleaning up temporary folders... "
 rm -rf "$STAGING_DIR"
